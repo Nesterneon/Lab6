54ca1ee491eee36638bd709b9e1085b3f29de55a
79064 <79064@MSI>
2021-11-20 22:34:01 +0300

    Добавьте файлы проекта.

---
+++ Lab6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите фразу: ");
+            String text = Console.ReadLine();
+            String[] words = text.Split();
+            int maxword = 0;
+            int index = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > maxword)
+                {
+                    maxword = words[i].Length;
+                    index = i;
+                }
+            }
+            Console.WriteLine("Самое длинное слово: {0}", words[index]);
+            Console.WriteLine("Для завершения нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Palindrome/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            string text= Console.ReadLine();
+            String[] words = text.Split();
+            String result = "";
+            foreach (string s in words) 
+            {
+                result += s.ToLower();
+            }
+            String reversetext = "";
+            for (int i = result.Length - 1; i >= 0; i--)
+            {
+                reversetext += result[i];
+            }
+            if (result == reversetext)
+                Console.WriteLine("введенная строка является палиндромом.");
+            else
+                Console.WriteLine("Введенная строка не является палиндромом.");
+            Console.WriteLine("Для завершения нажмите любую клавишу");
+            Console.ReadKey();
+        }  
+    }
+}
